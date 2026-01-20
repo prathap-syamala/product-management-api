e682d79b0c2e2e53d105b2dfc04c59d1a7424f0d
PrathapReddy S <prathapsyamala03@gmail.com>
2026-01-20 21:00:13 +0530

    product changes

--- DTOs/Product/CreateProductDto.cs
+++ DTOs/Product/CreateProductDto.cs
@@ -9,6 +9,21 @@ namespace ProductApi.DTOs.Products
         public string Name { get; set; }
 
         [Required]
+        [MaxLength(50)]
+        public string ProductCode { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Manufacturer { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        [Url]
+        [MaxLength(500)]
+        public string ImageUrl { get; set; }
+
+        [Required]
         [Range(0.01, 999999)]
         public decimal Price { get; set; }
 
